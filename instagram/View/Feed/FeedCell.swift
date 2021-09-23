1aa4bbf6c09d99aaa76b1b7c7d30cca1c982c922
sotomaque <sotomaque@gmail.com>
2021-09-22 19:51:33 -0700

    refined feed cell styles;

--- instagram/View/Feed/FeedCell.swift
+++ instagram/View/Feed/FeedCell.swift
@@ -22,6 +22,7 @@ struct FeedCell: View {
                 Text("Enrique")
                     .font(.system(size: 14, weight: .semibold))
             }
+            .padding([.leading, .bottom], 8)
             // Image
             Image("seattle-2")
                 .resizable()
@@ -29,7 +30,7 @@ struct FeedCell: View {
                 .frame(maxHeight: 440)
                 .clipped()
             // Action Buttons
-            HStack(spacing: 8) {
+            HStack(spacing: 16) {
                 // Like Button
                 Button {
                     
@@ -37,7 +38,7 @@ struct FeedCell: View {
                     Image(systemName: "heart")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 28, height: 28)
+                        .frame(width: 20, height: 20)
                         .font(.system(size: 20))
                         .padding(4)
                       
@@ -49,7 +50,7 @@ struct FeedCell: View {
                     Image(systemName: "bubble.right")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 28, height: 28)
+                        .frame(width: 20, height: 20)
                         .font(.system(size: 20))
                         .padding(4)
                       
@@ -61,22 +62,30 @@ struct FeedCell: View {
                     Image(systemName: "paperplane")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 28, height: 28)
+                        .frame(width: 20, height: 20)
                         .font(.system(size: 20))
                         .padding(4)
                       
                 }
             }
             .foregroundColor(.black)
+            .padding(.leading, 4)
+            
             // Caption + Details
+            Text("3 likes").font(.system(size: 14, weight: .semibold))
+                .padding(.leading, 4)
+                .padding(.bottom, 2)
+            
             HStack {
                 Text("Batman").font(.system(size: 14, weight: .semibold)) + Text(" All men have limimtes. They learn what they are and learn not to exceed them. I ignore mine")
-                
-                Text("2d")
-                    .font(.system(size: 14))
-                    .foregroundColor(.gray)
-                    .padding(.top)
             }
+            .padding(.horizontal, 8)
+            
+            Text("2d")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+                .padding(.leading, 8)
+                .padding(.top, 2)
         }
     }
 }
