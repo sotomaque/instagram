943bb1b64beff1d089650bb917977e7e16972a0d
sotomaque <sotomaque@gmail.com>
2021-09-22 18:49:47 -0700

    Initial Commit

---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Enrique Sotomayor on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
